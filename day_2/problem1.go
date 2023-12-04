10aeb9dbfe9aade9b95ca604017868b763d36bb0
Sarath <vssarathc04@gmail.com>
2023-12-04 11:47:00 +0530

    +read file for prob2

--- day_2/problem1.go
+++ day_2/problem1.go
@@ -1,4 +1,37 @@
-package day2
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type Color int
+
+const (
+	Red Color = iota
+	Green
+	Blue
+)
 
 func main() {
+	filePath := os.Args[1]
+	fileLines := readFile(filePath)
+}
+
+func readFile(filePath string) []string {
+	readFile, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("Error opening file")
+		panic(err)
+	}
+	defer readFile.Close()
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	var fileLines []string
+	for fileScanner.Scan() {
+		fileLines = append(fileLines, fileScanner.Text())
+	}
+	return fileLines
 }
