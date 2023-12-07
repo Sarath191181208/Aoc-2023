3d51b5c4f7f775fa5f3a396acddaf864ba4b2f2b
Sarath <vssarathc04@gmail.com>
2023-12-07 15:05:20 +0530

    prob 6 part 1 completed

--- day_6/main.go
+++ day_6/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -12,6 +15,40 @@ func main() {
 }
 
 func solveProblem1(fileContents []string) {
+	times := readSpaceDelimeterdInts(strings.Split(fileContents[0], ":")[1])
+	distances := readSpaceDelimeterdInts(strings.Split(fileContents[1], ":")[1])
+	mul := 1
+	for i, time := range times {
+		mul *= calcNumRecordBreaks(time, distances[i])
+	}
+	fmt.Print(mul)
+}
+
+func calcNumRecordBreaks(time int, distance int) int {
+	sum := 0
+	for i := 0; i < time; i++ {
+		num := i * (time - i)
+		if num > distance {
+			sum += 1
+		}
+	}
+	return sum
+}
+
+func readSpaceDelimeterdInts(s string) []int {
+	strArr := strings.Split(strings.TrimSpace(s), " ")
+	var numsArr []int
+	for _, val := range strArr {
+		if val == "" {
+			continue
+		}
+		num, err := strconv.Atoi(strings.TrimSpace(val))
+		if err != nil {
+			panic("Can't convert" + val + " into num, Failed!")
+		}
+		numsArr = append(numsArr, num)
+	}
+	return numsArr
 }
 
 func readFileFromPath(filePath string) []string {
